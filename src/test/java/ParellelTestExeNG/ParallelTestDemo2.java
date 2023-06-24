e7a84ef2074554435d4f479532a428ff59b6adee
GaneshPabale1696 <GaneshPabale1696@example.com>
2023-06-24 04:06:03 -0700

    This is my first command

---
+++ src/test/java/ParellelTestExeNG/ParallelTestDemo2.java
@@ -0,0 +1,43 @@
+package ParellelTestExeNG;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ParallelTestDemo2 {
+
+	@Test
+	public void verifyLogin()
+	{
+		
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions ops = new ChromeOptions();
+		ops.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(ops); // launch chrome
+		
+		driver.get("https://www.saucedemo.com/");
+		
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		
+		driver.findElement(By.id("login-button")).click();
+		
+		Assert.assertEquals(driver.getTitle(), "Swag Labs");
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
+	
+	
+}
